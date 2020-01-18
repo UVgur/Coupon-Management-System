5bb065f9e04add2fd2feca8a3e667e11bdb7cd9b
Yuval <uvgur931@gmail.com>
2020-01-18 02:19:07 +0200

    33

--- src/main/java/coupon_system/utilities/LoginFilter.java
+++ src/main/java/coupon_system/utilities/LoginFilter.java
@@ -29,6 +29,15 @@ public class LoginFilter extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
+
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addIntHeader("Access-Control-Max-Age", 10);
+
+
         String path = req.getRequestURL().toString();
         if (!path.contains("login")) {
             if (!path.contains("registration")) {
